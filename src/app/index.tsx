256ad313ce4030c99048ce8b9ad006d7a98c74d3
Evan Silva <evanildosilva467@gmail.com>
2025-10-11 00:47:49 -0300

    Tela inicial criada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,61 @@
-import { theme } from "@/theme/theme";
-import { Text, View } from "react-native";
+import { theme } from '@/theme/theme';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+// Definir Dimensions
+const { width, height } = Dimensions.get('screen')
+
+// Soucer de video 
+const videoSource = require('@/assets/media/background.mp4')
 
 export default function App() {
+
+    // Cria o useVideoPlayer
+    const player = useVideoPlayer(videoSource, (player) => {
+        player.loop = true
+        player.play()
+        player.muted = true
+        // otimizar o carregamento e cache
+        player.allowsExternalPlayback = false;
+    })
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{color: theme.colors.red[300],
-                fontFamily: theme.fonts.title,
-                fontSize: theme.textSizes.title
-            }}>SILKSONG</Text>
-            <Text style={{
-                color: theme.colors.black[100],
-                fontFamily: theme.fonts.text,
-                fontSize: theme.textSizes.large,
-                marginTop: theme.spacing.sm,
-            }}>starting</Text>
-            <Text style={{
-                color: theme.colors.gray[100],
-                fontFamily: theme.fonts.italic,
-                fontSize: theme.textSizes.small,
-                marginTop: theme.spacing.xs,
-            }}>project</Text>
+        <View style={styles.container}>
+            <VideoView
+                player={player}
+                style={styles.video}
+                contentFit="fill"
+                nativeControls={false}
+            />
+            <View style={styles.contenOverlay}>
+                <Text style={styles.text}>SEGUE</Text>
+                <Text style={styles.text}>CURTE</Text>
+                <Text style={styles.text}>COMPARTILHA</Text>
+            </View>
         </View>
     )
-}         
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    video: {
+        position: 'absolute',
+        width,
+        height,
+        top: 0,
+        left: 0,
+        zIndex: -1,
+    },
+    contenOverlay: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'transparent'
+    },
+    text: {
+        fontFamily: theme.fonts.title,
+        fontSize: theme.textSizes.title,
+        color: theme.colors.white[100],
+    }
+})
